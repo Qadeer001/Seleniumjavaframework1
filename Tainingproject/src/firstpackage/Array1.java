9d71b8dc30612d2759e94ab9bfdfdf45fd8daa9c
Qadeer001 <66002717+Qadeer001@users.noreply.github.com>
2020-06-13 21:26:07 +0530

    Changed values of the Array

--- Tainingproject/src/firstpackage/Array1.java
+++ Tainingproject/src/firstpackage/Array1.java
@@ -4,7 +4,7 @@ public class Array1 {
 	
 	public static void main(String[] args){
 		
-		int a[]={10,20,30,40,50};
+		int a[]={20,30,40,60,50};
 		int i;
 		int size=a.length-1;
 		for(i=0;i<=size;i++){
